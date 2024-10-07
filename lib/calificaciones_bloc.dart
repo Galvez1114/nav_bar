351206cde66c803d866cdffe0d86ac65942ba255
Galvez1114 <l20441114@mochis.tecnm.mx>
2024-10-07 08:07:40 -0700

    primeros pasos

---
+++ lib/calificaciones_bloc.dart
@@ -0,0 +1,78 @@
+import 'dart:collection';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CalificacionesBloc
+    extends Bloc<EventoCalificacion, EstadoCalificaciones> {
+  final List<String> _alumnos = [
+    'Juan',
+    'Frnacisco',
+    'Paco',
+    'Pancho',
+    'Curro',
+    'Cisco',
+    'Kiko'
+  ];
+
+  final List<String> _aprobados = [];
+  List<String> get aprobados => UnmodifiableListView(_aprobados);
+  final List<String> _reprobados = [];
+
+  List<String> get alumnos => UnmodifiableListView(_alumnos);
+  int _indice = 0;
+  int get indice => _indice;
+
+  CalificacionesBloc() : super(EstadoInicial()) {
+    on<CambioTab>((event, emit) {
+      _indice = event.indice;
+      emit(NuevoTab(indice: _indice));
+    });
+    on<Aprobado>((event, emit) {
+      _aprobados.add(event.nombre);
+      _alumnos.removeWhere((element) => element == event.nombre);
+      emit(CambioAlumno(nombre: event.nombre));
+    });
+  }
+}
+
+sealed class EventoCalificacion {}
+
+class CambioTab extends EventoCalificacion {
+  final int indice;
+
+  CambioTab({required this.indice});
+}
+
+sealed class EstadoCalificaciones {}
+
+class NuevoTab extends EstadoCalificaciones {
+  final int indice;
+
+  NuevoTab({required this.indice});
+}
+
+class CambioAlumno extends EstadoCalificaciones {
+  final String nombre;
+
+  CambioAlumno({required this.nombre});
+}
+
+abstract class EventoAlumno extends EventoCalificacion {
+  final String nombre;
+
+  EventoAlumno({required this.nombre});
+}
+
+class Aprobado extends EventoAlumno {
+  Aprobado({required super.nombre});
+}
+
+class Reprobado extends EventoAlumno {
+  Reprobado({required super.nombre});
+}
+
+class Revision extends EventoAlumno {
+  Revision({required super.nombre});
+}
+
+class EstadoInicial extends EstadoCalificaciones {}
---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nav_bar/calificaciones_bloc.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (context) => CalificacionesBloc(),
+        child: BlocBuilder<CalificacionesBloc, EstadoCalificaciones>(
+          builder: (context, state) {
+            var estado = context.watch<CalificacionesBloc>();
+            return Scaffold(
+              bottomNavigationBar: BarraNavegacion(indice: estado.indice),
+              body: Center(
+                child: switch (estado.indice) {
+                  0 => ListaPorCalificar(alumnos: estado.alumnos),
+                  1 => ListaAprobados(alumnos: estado.aprobados),
+                  _ => const Advertencia(),
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Advertencia extends StatelessWidget {
+  const Advertencia({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('No debes estar aqui');
+  }
+}
+
+class ListaPorCalificar extends StatelessWidget {
+  final List<String> alumnos;
+  const ListaPorCalificar({super.key, required this.alumnos});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: alumnos.length,
+      itemBuilder: (context, index) => Elemento(alumno: alumnos[index]),
+    );
+  }
+}
+
+class ListaAprobados extends StatelessWidget {
+  final List<String> alumnos;
+  const ListaAprobados({super.key, required this.alumnos});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: alumnos.length,
+      itemBuilder: (context, index) => Elemento(alumno: alumnos[index]),
+    );
+  }
+}
+
+class Elemento extends StatelessWidget {
+  final String alumno;
+  const Elemento({super.key, required this.alumno});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      direction: DismissDirection.horizontal,
+      background: const ColoredBox(
+        color: Colors.green,
+        child: Text('Aprobarlo'),
+      ),
+      secondaryBackground: const ColoredBox(
+        color: Colors.yellow,
+        child: Text('Reprobarlo'),
+      ),
+      onDismissed: (direction) {
+        if (direction == DismissDirection.startToEnd) {
+          context.read<CalificacionesBloc>().add(Aprobado(nombre: alumno));
+        }
+        if (direction == DismissDirection.endToStart) {
+          context.read<CalificacionesBloc>().add(Reprobado(nombre: alumno));
+        }
+      },
+      key: UniqueKey(),
+      child: ListTile(
+        title: Text(alumno),
+        style: ListTileStyle.list,
+      ),
+    );
+  }
+}
+
+class BarraNavegacion extends StatelessWidget {
+  final int indice;
+  const BarraNavegacion({super.key, required this.indice});
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+        indicatorColor: Colors.purple,
+        selectedIndex: indice,
+        onDestinationSelected: (value) {
+          context.read<CalificacionesBloc>().add(CambioTab(indice: value));
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.alarm), label: 'Por revisar'),
+          NavigationDestination(icon: Icon(Icons.home), label: 'Aprobados'),
+          NavigationDestination(icon: Icon(Icons.error), label: 'Reprobados')
+        ]);
+  }
+}
